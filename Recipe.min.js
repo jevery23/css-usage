37a03bfec09d87079f3986a7723c0d23faa75cad
John Every <joevery@microsoft.com>
2018-02-01 12:14:45 -0800

    Forgot to update Recipe.min.js.

--- Recipe.min.js
+++ Recipe.min.js
@@ -1781,102 +1781,102 @@ void function() { try {
 	
 } catch (ex) { /* do something maybe */ throw ex; } }();
 
-/* 
-    RECIPE: Pointer events and touch events listening counter
-    -------------------------------------------------------------
-    Author: joevery
-    Description: Find instances of listening for pointer and touch events.
-*/
+///* 
+//    RECIPE: Pointer events and touch events listening counter
+//    -------------------------------------------------------------
+//    Author: joevery
+//    Description: Find instances of listening for pointer and touch events.
+//*/
 
-void function ()
-{
-    window.CSSUsage.StyleWalker.recipesToRun.push(
-        function pointer_events_touch_events(/*HTML DOM Element*/ element, results)
-        {
-            var nodeName = element.nodeName;
+//void function ()
+//{
+//    window.CSSUsage.StyleWalker.recipesToRun.push(
+//        function pointer_events_touch_events(/*HTML DOM Element*/ element, results)
+//        {
+//            var nodeName = element.nodeName;
 
-            // We want to catch all instances of listening for these events.
-            var eventsToCheckFor = ["pointerup", "pointerdown", "pointermove", "pointercancel", "pointerout", "pointerleave", "pointerenter", "pointerover",
-                "touchstart", "touchend", "touchmove", "touchcancel"];
+//            // We want to catch all instances of listening for these events.
+//            var eventsToCheckFor = ["pointerup", "pointerdown", "pointermove", "pointercancel", "pointerout", "pointerleave", "pointerenter", "pointerover",
+//                "touchstart", "touchend", "touchmove", "touchcancel"];
 
-            var JsTypes =
-                {
-                    ATTRIBUTE: 1,
-                    INTERNAL: 2,
-                    EXTERNAL: 3,
-                }
+//            var JsTypes =
+//                {
+//                    ATTRIBUTE: 1,
+//                    INTERNAL: 2,
+//                    EXTERNAL: 3,
+//                }
 
-            var jsType;
+//            var jsType;
 
-            // Is element a script tag?
-            if (nodeName === "SCRIPT") {
-                // If no text, then it cannot be an internal script.
-                if (element.text !== undefined && element.text !== "") {
-                    jsType = JsTypes.INTERNAL;
-                }
-                // if no source, then it cannot be an external script. 
-                else if (element.src !== undefined) {
-                    // if external script, then we have to go and get it, if it is not our recipe script or the src is not blank.
-                    if (element.src.includes("Recipe.min.js") || element.src === "") {
-                        return results;
-                    }
-                    else {
-                        jsType = JsTypes.EXTERNAL;
+//            // Is element a script tag?
+//            if (nodeName === "SCRIPT") {
+//                // If no text, then it cannot be an internal script.
+//                if (element.text !== undefined && element.text !== "") {
+//                    jsType = JsTypes.INTERNAL;
+//                }
+//                // if no source, then it cannot be an external script. 
+//                else if (element.src !== undefined) {
+//                    // if external script, then we have to go and get it, if it is not our recipe script or the src is not blank.
+//                    if (element.src.includes("Recipe.min.js") || element.src === "") {
+//                        return results;
+//                    }
+//                    else {
+//                        jsType = JsTypes.EXTERNAL;
 
-                        var xhr = new XMLHttpRequest();
-                        xhr.open("GET", element.src, false);
-                        xhr.send();
-                        if (xhr.status !== 200) {
-                            // We no longer want to check this element if there was a problem in making request.
-                            return results;
-                        }
-                    }
-                }
-            }
-            // If element is not a script tag, then we will assume that if listening for pointerevents is present that it will be in the form of an attribute.
-            else {
-                jsType = JsTypes.ATTRIBUTE;
-            }
+//                        var xhr = new XMLHttpRequest();
+//                        xhr.open("GET", element.src, false);
+//                        xhr.send();
+//                        if (xhr.status !== 200) {
+//                            // We no longer want to check this element if there was a problem in making request.
+//                            return results;
+//                        }
+//                    }
+//                }
+//            }
+//            // If element is not a script tag, then we will assume that if listening for pointerevents is present that it will be in the form of an attribute.
+//            else {
+//                jsType = JsTypes.ATTRIBUTE;
+//            }
 
-            for (const event of eventsToCheckFor) {
-                switch (jsType) {
-                    case JsTypes.ATTRIBUTE:
-                        // Attribute specified on element does not seem to work at present, but checking anyway.
-                        if (element.attributes["on" + event] !== undefined) {
-                            results[event] = results[event] || { count: 0, };
-                            results[event].count++;
-                        }
-                        break;
+//            for (const event of eventsToCheckFor) {
+//                switch (jsType) {
+//                    case JsTypes.ATTRIBUTE:
+//                        // Attribute specified on element does not seem to work at present, but checking anyway.
+//                        if (element.attributes["on" + event] !== undefined) {
+//                            results[event] = results[event] || { count: 0, };
+//                            results[event].count++;
+//                        }
+//                        break;
 
-                    case JsTypes.INTERNAL:
-                        // Check for one instance if none present then abandon.
-                        if (element.text.indexOf(event) !== -1) {
-                            results[event] = results[event] || { count: 0, };
-                            results[event].count += findNumOfStringInstancesInText_CaseSensitive(event, element.text);
-                        }
-                        break;
+//                    case JsTypes.INTERNAL:
+//                        // Check for one instance if none present then abandon.
+//                        if (element.text.indexOf(event) !== -1) {
+//                            results[event] = results[event] || { count: 0, };
+//                            results[event].count += findNumOfStringInstancesInText_CaseSensitive(event, element.text);
+//                        }
+//                        break;
 
-                    case JsTypes.EXTERNAL:
-                        // Check for one instance if none present then abandon.
-                        if (xhr.responseText.indexOf(event) !== -1) {
-                            results[event] = results[event] || { count: 0, };
-                            results[event].count += findNumOfStringInstancesInText_CaseSensitive(event, xhr.responseText);
-                        }
-                        break;
-                }
-            }
+//                    case JsTypes.EXTERNAL:
+//                        // Check for one instance if none present then abandon.
+//                        if (xhr.responseText.indexOf(event) !== -1) {
+//                            results[event] = results[event] || { count: 0, };
+//                            results[event].count += findNumOfStringInstancesInText_CaseSensitive(event, xhr.responseText);
+//                        }
+//                        break;
+//                }
+//            }
 
-            return results;
-        });
+//            return results;
+//        });
 
-    function findNumOfStringInstancesInText_CaseSensitive(string, text)
-    {
-        var regex = new RegExp(string, 'g');
-        var instances = text.match(regex);
+//    function findNumOfStringInstancesInText_CaseSensitive(string, text)
+//    {
+//        var regex = new RegExp(string, 'g');
+//        var instances = text.match(regex);
 
-        return instances.length;
-    }
-}();
+//        return instances.length;
+//    }
+//}();
 /* 
     RECIPE: Pointer events and touch events listening counter
     -------------------------------------------------------------
